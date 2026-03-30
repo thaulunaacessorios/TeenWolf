abdd81c4094ba17315854b3a23541a82fdd4e27c
Thau Luna <thauluna.acessorios@gmail.com>
2026-03-30 16:05:09 -0300

    Add custom cursor effects on mouse movement

--- script.js
+++ script.js
@@ -491,3 +491,21 @@ function toggleTheme(){
 }
 
 window.addEventListener('load', initPage);
+
+const cursor = document.getElementById("cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
+const links = document.querySelectorAll("a, button");
+
+links.forEach(el => {
+  el.addEventListener("mouseenter", () => {
+    cursor.style.transform = "translate(-50%, -50%) scale(1.5)";
+  });
+
+  el.addEventListener("mouseleave", () => {
+    cursor.style.transform = "translate(-50%, -50%) scale(1)";
+  });
+});
